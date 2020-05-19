6594c41bba6d38f23d7005c0ba1b66f46e7cd994
Jonathan <jonathanthornton1997@gmail.com>
2020-05-18 17:03:25 -0700

    Finished MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  
+  const urlID = useParams()
+  
   useEffect(() => {
-    const id = 1;
+    const id = urlID;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
+    console.log(id)
 
        axios
-        .get(`http://localhost:5000/api/movies/${id}`)
+        .get(`http://localhost:5000/api/movies/${id.movieID}`)
         .then(response => {
           setMovie(response.data);
         })
@@ -18,7 +22,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[urlID]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link, useRouteMatch, useParams } from 'react-router-dom'
 const MovieList = props => {
   return (
     <div className="movie-list">
@@ -10,10 +10,24 @@ const MovieList = props => {
   );
 }
 
+
+
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
+  const { url } = useRouteMatch()
+
+  // const {movieID} = useParams()
+  // const item = movie.find(mv => {
+  //   return movieID === mv.id.toString()
+  // })
+
+
+
   return (
+  
     <div className="movie-card">
+
+      <Link to={`/movies/${movie.id}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -22,12 +36,14 @@ function MovieDetails({ movie }) {
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
-
+      </Link>
       {stars.map(star => (
         <div key={star} className="movie-star">
-          {star}
+          {star}  
         </div>
+        
       ))}
+      
     </div>
   );
 }
